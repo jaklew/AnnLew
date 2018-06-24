e4dce8942f1db31037c3cbd574bd5e1b09c39703
Jakub Lewandowski <kubik92@gmail.com>
2018-06-24 10:40:29 +0200

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+//checking for the correct date to show vacation message
+$(document).ready(function(){
+    $('#hr3').hide();
+    $('#row3').hide();
+    var currentDate = new Date()
+    var expiryDate = new Date("July 1, 2018 00:00:00");
+    if(currentDate.getTime() > expiryDate.getTime()){
+        $('#hr3').show();
+        $('#row3').show();
+    }
+    console.log("hehe");
+})
